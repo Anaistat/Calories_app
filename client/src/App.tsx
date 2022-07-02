b1a59839542557e74e3e65cf1ab42f716563dc69
“Tanya <tanyazverevaa@gmail.com>
2022-07-02 18:22:09 +0300

    add eslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
 
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Pages/Questions/Questions.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+
+interface QuestionsProps{
+    questions: string[],
+    link: string
+}
+
+const Questions:FC<QuestionsProps> = ({}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Questions;
